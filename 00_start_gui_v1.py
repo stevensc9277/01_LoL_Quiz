68735a6fa601830d26f1cd64a0e2e80afcab823d
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-04-06 12:15:38 +1200

    made starting frame, currently empty

---
+++ 00_start_gui_v1.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+
+class Start:
+    def __init__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("League of Legends Champions")
+    something = Start(root)
+    root.mainloop()
